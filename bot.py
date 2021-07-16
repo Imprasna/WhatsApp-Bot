0ad39a67519ee16e50d24bf091e0ed0891fefc68
Imprasna <31104095+Imprasna@users.noreply.github.com>
2021-07-17 00:57:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,3 @@
 import pywhatkit as kit
 
-kit.sendwhatmsg('+918111060010', 'I am playing with bot da so don mind this', 00, 52, print_wait_time='true', tab_close='true')
+kit.sendwhatmsg('+918111060010', 'I am playing with bot da so don mind this', 00, 52, print_wait_time=10, tab_close='true')
